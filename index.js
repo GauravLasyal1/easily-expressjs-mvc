fe82054968fa155022ada6c163386e48df2dd58c
Gaurav Lasyal <lasyalgaurav@gmail.com>
2024-10-28 12:02:24 +0530

    navbar added in layout.ejs

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from "express";
+import expressEjsLayouts from "express-ejs-layouts";
+import path from "path";
 
 
 const server = express();
 
+server.set("view engine", "ejs");
+server.set("views", path.resolve("src", "views"));
+
+
+server.use(expressEjsLayouts);
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static("public"));
+
 server.get("/", (req, res) => {
-    res.send("Hello World");
+    res.render("home");
 });
 
 server.listen(3000, () => {
